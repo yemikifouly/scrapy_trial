8efd6c001a56dc86f4fc71c88e151f9c10a517ad
Yemi <ybkifouly@gmail.com>
2020-10-03 00:14:26 +0100

    Initial commit

---
+++ animalscrape/animalscrape/spiders/animals_spider.py
@@ -0,0 +1,36 @@
+import scrapy
+import json
+
+class AnimalsSpider(scrapy.Spider):
+
+    name = 'animals'
+    
+    start_urls = [
+        'https://lib2.colostate.edu/wildlife/atoz.php?sortby=Common_Names&letter=ALL'
+    ]
+    
+    def parse(self, response):
+    
+        animals = dict()
+        animals['name'] = 'sorted animal & species'
+        animals['letters'] = {}
+        
+        # Iterate over all the rows of the table of animals
+        for post in response.css('div.tab table.names tr'):
+        
+            # Get the names and the species (the first link is the name, the second the species)
+            common_name, genus_species = post.css('a::text')[0].get(), post.css('a::text')[1].get()
+            
+            # ------- The scraping happens above. Everything below is getting the data into a dict ------
+            
+            letter = common_name[0]
+            # Initialize the list of animals and the total if we are dealing with a new letter
+            if letter not in animals['letters']:
+                animals['letters'][letter] = {'total': 0, 'animals':[]}
+            
+            # Add animal name and species to the list of animals whose name starts with the same letter
+            animals['letters'][letter]['total'] += 1
+            animals['letters'][letter]['animals'].append({'name(s)': common_name, 'species': genus_species})
+            
+        with open('result.json', 'w') as f:
+            json.dump(animals, f)
